d11696c9f474e527a5a9b41b59f64eb3b2cb9a53
Nathan Sikkema <nathan.sikkema@mohawkcollege.ca>
2025-04-09 15:11:39 -0400

    Add initial HTML, CSS, and JavaScript files for Math Quiz project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Author............ : Nathan Sikkema, Divine Imodunbi, Souvanno Souvannasane
+// Student Number.... : 000911571, 
+// Program........... : Software Development
+// Course............ : Software Development Tools
+// Type.............. : HMTL
+// Start Date........ : April 09, 2025
+// End Date.......... : April 09, 2025
